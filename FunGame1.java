9bf17e6ef9594943f7c7639de38aa883b340a67c
Aditya Yadav <adityaraj8433199@gmail.com>
2025-08-17 18:01:12 +0530

    FunGame1.java

--- FunGame1.java
+++ FunGame1.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
+import java.util.Random;
 public class FunGame1 {
     Scanner io = new Scanner(System.in);
+    Random ran = new Random();
 
     void guessGame(){
-
-        byte winNumber = 73;
-        System.out.println("[Enter number between 1 to 100]");
+        int winNumber = ran.nextInt(5) + 1;
+        System.out.println("[Enter number between 1 to 5]");
         System.out.print("Enter number: ");
         byte guessNumber = io.nextByte();
-
-        //Conditional Statement
-
+        //verification
+        if((guessNumber<1) || (guessNumber>5)){
+            System.out.println("Invalid input! Please enter a number between 1 and 5.");
+        }
+        //game logic
         if(guessNumber == winNumber){
             System.out.println("You nailed it! You're a genius.");
         } else if(guessNumber < winNumber){
-            System.out.println("\uD83D\uDCC9 Too low! Try aiming higher.");
+            System.out.println("Too low! Try aiming higher.");
         } else {
-            System.out.println("\uD83D\uDCC8 Too high! Try aiming lower.");
+            System.out.println("Too high! Try aiming lower.");
         }
 
     }
